ca73d4e53e8c5367941b9a222280649eddbf9871
Jeroham Sanchez <jeroham.sanchez@gmail.com>
2024-12-27 16:54:30 -0500

    S10-107: stripe configured

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -3,17 +3,17 @@ import * as joi from 'joi';
 
 interface EnvVars {
   PORT: number;
+  STRIPE_SECRET_KEY: string;
 }
 
 const envSchemaValidator = joi
   .object({
     PORT: joi.number().required(),
+    STRIPE_SECRET_KEY: joi.string().required(),
   })
   .unknown(true);
 
-const { error, value } = envSchemaValidator.validate({
-  ...process.env,
-});
+const { error, value } = envSchemaValidator.validate(process.env);
 
 if (error) {
   throw new Error(`Payments Config(.env) has error: ${error.message}`);
@@ -23,4 +23,5 @@ const envVars: EnvVars = value;
 
 export const envs = {
   port: envVars.PORT,
+  STRIPE_SECRET_KEY: envVars.STRIPE_SECRET_KEY,
 };
