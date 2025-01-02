5bfb5b1161b2ddf8bd119d3e60d83d5843ca4f21
Jeroham Sanchez <jeroham.sanchez@gmail.com>
2025-01-02 10:57:28 -0500

    S10-112: hookdeck - event gateway-forwader

--- src/payments/payments.service.ts
+++ src/payments/payments.service.ts
@@ -58,22 +58,28 @@ export class PaymentsService {
         return response.sendStatus(400);
       }
       switch (event.type) {
-        case 'payment_intent.succeeded':
-          const paymentIntent = event.data.object;
-          console.log(
-            `PaymentIntent for ${paymentIntent.amount} was successful!`,
-          );
-          // Then define and call a method to handle the successful payment intent.
-          // handlePaymentIntentSucceeded(paymentIntent);
-          break;
-        case 'payment_method.attached':
+        // case 'payment_intent.succeeded':
+        //   const paymentIntent = event.data.object;
+        //   console.log(
+        //     `PaymentIntent for ${paymentIntent.amount} was successful!`,
+        //   );
+        //   // Then define and call a method to handle the successful payment intent.
+        //   // handlePaymentIntentSucceeded(paymentIntent);
+        //   break;
+        // case 'payment_method.attached':
+        //   const paymentMethod = event.data.object;
+        //   // Then define and call a method to handle the successful attachment of a PaymentMethod.
+        //   // handlePaymentMethodAttached(paymentMethod);
+        //   break;
+        case 'charge.updated':
           const paymentMethod = event.data.object;
           // Then define and call a method to handle the successful attachment of a PaymentMethod.
           // handlePaymentMethodAttached(paymentMethod);
+          console.log(`OK: ${event.type}. Succesfully!`);
           break;
         default:
           // Unexpected event type
-          console.log(`Unhandled event type ${event.type}.`);
+          console.log(`!!Unhandled event type ${event.type}.`);
       }
 
       // Return a 200 response to acknowledge receipt of the event
