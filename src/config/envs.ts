da15fbcd175c5bc3434f1237de7e1ba8f5f82ece
Jeroham Sanchez <jeroham.sanchez@gmail.com>
2025-01-02 09:53:38 -0500

    S10-111: webhook cli implemented

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -4,12 +4,14 @@ import * as joi from 'joi';
 interface EnvVars {
   PORT: number;
   STRIPE_SECRET_KEY: string;
+  STRIPE_WEBHOOK_SECRET: string;
 }
 
 const envSchemaValidator = joi
   .object({
     PORT: joi.number().required(),
     STRIPE_SECRET_KEY: joi.string().required(),
+    STRIPE_WEBHOOK_SECRET: joi.string().required(),
   })
   .unknown(true);
 
@@ -24,4 +26,5 @@ const envVars: EnvVars = value;
 export const envs = {
   port: envVars.PORT,
   STRIPE_SECRET_KEY: envVars.STRIPE_SECRET_KEY,
+  STRIPE_WEBHOOK_SECRET: envVars.STRIPE_WEBHOOK_SECRET,
 };
--- src/payments/payments.service.ts
+++ src/payments/payments.service.ts
@@ -7,6 +7,7 @@ import { Request, Response } from 'express';
 @Injectable()
 export class PaymentsService {
   private readonly stripe = new Stripe(envs.STRIPE_SECRET_KEY);
+
   async createSession(paymentSessionDto: PaymentSessionDto) {
     try {
       const { currency, items } = paymentSessionDto;
@@ -42,9 +43,41 @@ export class PaymentsService {
 
   async paymentWebhook(request: Request, response: Response) {
     try {
-      const event = request.body;
-      console.log('TYPE:', event.type);
-      console.log('OBJECT:', event);
+      //* ver documentación: https://docs.stripe.com/webhooks/quickstart
+      let event = request.body;
+      const signature = request.headers['stripe-signature'];
+
+      try {
+        event = this.stripe.webhooks.constructEvent(
+          request['rawBody'],
+          signature,
+          envs.STRIPE_WEBHOOK_SECRET,
+        );
+      } catch (err) {
+        console.log(`⚠️  Webhook signature verification failed.`, err.message);
+        return response.sendStatus(400);
+      }
+      switch (event.type) {
+        case 'payment_intent.succeeded':
+          const paymentIntent = event.data.object;
+          console.log(
+            `PaymentIntent for ${paymentIntent.amount} was successful!`,
+          );
+          // Then define and call a method to handle the successful payment intent.
+          // handlePaymentIntentSucceeded(paymentIntent);
+          break;
+        case 'payment_method.attached':
+          const paymentMethod = event.data.object;
+          // Then define and call a method to handle the successful attachment of a PaymentMethod.
+          // handlePaymentMethodAttached(paymentMethod);
+          break;
+        default:
+          // Unexpected event type
+          console.log(`Unhandled event type ${event.type}.`);
+      }
+
+      // Return a 200 response to acknowledge receipt of the event
+      response.send();
     } catch (error) {
       return new InternalServerErrorException(error);
     }
